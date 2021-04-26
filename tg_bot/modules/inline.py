774cc4425fe54180b6e54c47d23a8ace32b96eb0
rshero <52062460+rshero@users.noreply.github.com>
2021-04-26 08:10:16 -0700

    Update inline.py

--- tg_bot/modules/inline.py
+++ tg_bot/modules/inline.py
@@ -173,7 +173,7 @@ def inlineinfo(query: str, update: Update, context: CallbackContext) -> None:
         nation_level_present = True
 
     if nation_level_present:
-        text += ' [<a href="https://t.me/{}?start=nations">?</a>]'.format(bot.username)
+        text += ' [<a href="https://t.me/yuiichansupport/3655">?</a>]'.format(bot.username)
 
     try:
         spamwtc = sw.get_ban(int(user.id))
