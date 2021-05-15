d2b318f572cef1e2407a7027cc4c94e30fb84eef
rshero <52062460+rshero@users.noreply.github.com>
2021-05-15 05:07:08 -0700

    Update inline.py

--- tg_bot/modules/inline.py
+++ tg_bot/modules/inline.py
@@ -187,42 +187,6 @@ def inlineinfo(query: str, update: Update, context: CallbackContext) -> None:
             text += "<b>\n\n• SpamWatched:</b> No"
     except:
         pass  # don't crash if api is down somehow...
-
-    apst = requests.get(
-        f"https://api.intellivoid.net/spamprotection/v1/lookup?query={context.bot.username}"
-    )
-    api_status = apst.status_code
-    if api_status == 200:
-        try:
-            status = client.raw_output(int(user.id))
-            # ptid = status["results"]["private_telegram_id"]
-            op = status["results"]["attributes"]["is_operator"]
-            ag = status["results"]["attributes"]["is_agent"]
-            wl = status["results"]["attributes"]["is_whitelisted"]
-            ps = status["results"]["attributes"]["is_potential_spammer"]
-            sp = status["results"]["spam_prediction"]["spam_prediction"]
-            hamp = status["results"]["spam_prediction"]["ham_prediction"]
-            blc = status["results"]["attributes"]["is_blacklisted"]
-            if blc:
-                blres = status["results"]["attributes"]["blacklist_reason"]
-            else:
-                blres = None
-            text += "\n\n<b>SpamProtection:</b>"
-            # text += f"<b>\n• Private Telegram ID:</b> <code>{ptid}</code>\n"
-            text += f"<b>\n• Operator:</b> <code>{op}</code>\n"
-            text += f"<b>• Agent:</b> <code>{ag}</code>\n"
-            text += f"<b>• Whitelisted:</b> <code>{wl}</code>\n"
-            text += f"<b>• Spam/Ham Prediction:</b> <code>{round((sp/hamp*100), 3)}%</code>\n"
-            text += f"<b>• Potential Spammer:</b> <code>{ps}</code>\n"
-            text += f"<b>• Blacklisted:</b> <code>{blc}</code>\n"
-            text += f"<b>• Blacklist Reason:</b> <code>{blres}</code>\n"
-        except HostDownError:
-            text += "\n\n<b>SpamProtection:</b>"
-            text += "\nCan't connect to Intellivoid SpamProtection API\n"
-    else:
-        text += "\n\n<b>SpamProtection:</b>"
-        text += f"\n<code>API RETURNED: {api_status}</code>\n"
-
     num_chats = sql.get_user_num_chats(user.id)
     text += f"\n• Chat count: <code>{num_chats}</code>"
 
