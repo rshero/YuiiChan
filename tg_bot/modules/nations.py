d1e467d649ce2d47e8f1129d98ea7e3ae8e16f8d
rshero <52062460+rshero@users.noreply.github.com>
2021-06-08 19:35:21 +0530

    Update nations.py
    It's YuiChan Club now

--- tg_bot/modules/nations.py
+++ tg_bot/modules/nations.py
@@ -550,7 +550,7 @@ dispatcher.add_handler(SUPPORTLIST_HANDLER)
 dispatcher.add_handler(SUDOLIST_HANDLER)
 dispatcher.add_handler(DEVLIST_HANDLER)
 
-__mod_name__ = "Yuii"
+__mod_name__ = ""
 __handlers__ = [
     SUDO_HANDLER,
     SUPPORT_HANDLER,
